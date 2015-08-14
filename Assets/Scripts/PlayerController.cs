bea8f73e248837feee7bbbcf65756c9cc21ba68d
bq <daddatv@gmail.com>
2015-08-14 18:35:32 +0200

    fixed error and formatted code

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,8 +1,8 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class PlayerController : MonoBehaviour {
-
+public class PlayerController : MonoBehaviour
+{
 	Rigidbody2D rb;
 	bool grounded = false;
 	public float jumpForce = 30f;
@@ -10,33 +10,29 @@ public class PlayerController : MonoBehaviour {
 	public LayerMask whatIsGround;
 	public float moveSpeed = 10f;
 
-	// Use this for initialization
-	void Start () {
-
-		rb = GetComponent<Rigidbody2D> ();
-	
+	void Start()
+	{
+		rb = GetComponent<Rigidbody2D>();
 	}
-	
-	// Update is called once per frame
-	void Update () {
 
-		if (grounded && Input.GetButton (KeyCode.Space)) {
-			rb.AddForce (Vector2.up * jumpForce);
+	void Update()
+	{
+		if (grounded && Input.GetKeyDown(KeyCode.Space))
+		{
+			rb.AddForce(Vector2.up * jumpForce);
 			grounded = false;
 		}
 
-		if (Input.GetKey (KeyCode.A))
-			rb.velocity = new Vector2 (-moveSpeed, rb.velocity.y); 
+		if (Input.GetKey(KeyCode.A))
+			rb.velocity = new Vector2(-moveSpeed, rb.velocity.y);
 
-		if (Input.GetKey (KeyCode.D))
-			rb.velocity = new Vector2 (moveSpeed, rb.velocity.y);
-	
+		if (Input.GetKey(KeyCode.D))
+			rb.velocity = new Vector2(moveSpeed, rb.velocity.y);
 	}
 
-	void FixedUpdate () {
-
-		if (Physics2D.Raycast (feet.position, -Vector2.up, 2f, whatIsGround).collider != null)
+	void FixedUpdate()
+	{
+		if (Physics2D.Raycast(feet.position, -Vector2.up, 2f, whatIsGround).collider != null)
 			grounded = true;
-
 	}
 }
