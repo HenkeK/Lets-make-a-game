7efb439c3874d68fd5292e4c934cfe899d4fbf06
bq <daddatv@gmail.com>
2015-08-14 18:29:14 +0200

    started working on multiplayer
    also fixed added /Build folder to gitignore

---
+++ Assets/Scripts/Network/MultiplayerHandler.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MultiplayerHandler : MonoBehaviour
+{
+	private const string typeName = "BoxFighter";
+	private const string gameName = "Testing";
+	public int port = 25565;
+
+	void Start()
+	{
+		StartServer();
+	}
+
+	void Update()
+	{
+
+	}
+
+	private void StartServer()
+	{
+		Network.InitializeServer(2, port, !Network.HavePublicAddress());
+		MasterServer.RegisterHost(typeName, gameName);
+	}
+}
