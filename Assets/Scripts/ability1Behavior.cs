0f7a4a0674a5a877afb641d4f41c1770f94c78c9
HenkeBenke <kultala.henrik@gmail.com>
2015-08-20 19:23:59 +0200

    ...

--- Assets/Scripts/ability1Behavior.cs
+++ Assets/Scripts/ability1Behavior.cs
@@ -3,13 +3,21 @@ using System.Collections;
 
 public class ability1Behavior : MonoBehaviour {
 
+	float pushRadius = 4f;
+	float basePushForce = 300f;
+
 	// Use this for initialization
 	void Start () {
 
 		Destroy(gameObject, 3f);
 	
 	}
-	
+
+	void OnCollisionEnter2D(Collision2D coll)
+	{
+
+	}
+
 	// Update is called once per frame
 	void Update () {
 	
