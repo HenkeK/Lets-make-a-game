0966a59c85d9fb7162348b091457f80091e7d27b
HenkeBenke <kultala.henrik@gmail.com>
2015-08-20 22:26:34 +0200

    Added hearts to GUI and health script with "take damage" function

---
+++ Assets/Scripts/Health.cs
@@ -0,0 +1,43 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Health : MonoBehaviour {
+
+	int maxHealth = 5;
+	int currentHealth;
+	public TextAsset heart;
+	Texture2D heartTex = new Texture2D(10, 10);
+
+	// Use this for initialization
+	void Start () {
+		heartTex.LoadImage(heart.bytes);
+		currentHealth = maxHealth;
+		Invoke("TakeDamage",3);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (currentHealth<=0)
+		{
+			Destroy(gameObject);
+		}
+	}
+
+	void TakeDamage (int damage)
+	{
+		currentHealth -= damage;
+	}
+	/*void TakeDamage()
+	{
+		currentHealth--;
+		Debug.Log(currentHealth);
+	}*/
+
+	void OnGUI ()
+	{
+		for (int i = 1; i <= currentHealth; i++)
+		{
+			GUI.DrawTexture(new Rect(280 + 35 * i, 600, 30, 30), heartTex, ScaleMode.ScaleToFit);
+		}
+	}
+}
