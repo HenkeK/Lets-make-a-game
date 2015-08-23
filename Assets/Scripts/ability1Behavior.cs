73b20b0ab3010f6b7838830eb106c697c379bb28
HenkeBenke <kultala.henrik@gmail.com>
2015-08-23 18:09:16 +0200

    Knockback-shot made
    Needs balancing though; knockback search radius, initial force and fall
    off when further away. Not sure about but i think lowering initial force
    and lowering fall off might be good.
    On a side note: not sure whether it affects both players, dont think so
    but needs confirming

--- Assets/Scripts/ability1Behavior.cs
+++ Assets/Scripts/ability1Behavior.cs
@@ -3,22 +3,27 @@ using System.Collections;
 
 public class ability1Behavior : MonoBehaviour {
 
-	float pushRadius = 4f;
-	float basePushForce = 300f;
+	float pushRadius = 6f;
+	float basePushForce = 1000f;
+	Transform trans;
+	public LayerMask whatToPush;
 
-	// Use this for initialization
 	void Start () {
-
+		trans = GetComponent<Transform>();
 		Destroy(gameObject, 3f);
-	
 	}
 
-	void OnCollisionEnter2D(Collision2D coll)
-	{
-
+	void OnCollisionEnter2D(Collision2D coll) {
+		if (Physics2D.OverlapCircle(gameObject.transform.position, pushRadius, whatToPush)) {
+			GameObject player = Physics2D.OverlapCircle(gameObject.transform.position, pushRadius, whatToPush).gameObject;
+			float verticalPushForce = (player.transform.position.y - trans.position.y) / (Mathf.Abs(player.transform.position.x - trans.position.x) + Mathf.Abs(player.transform.position.y - trans.position.y));
+			float horizontalPushForce = (player.transform.position.x - trans.position.x)/ (Mathf.Abs(player.transform.position.x - trans.position.x) + Mathf.Abs(player.transform.position.y - trans.position.y));
+			Vector2 appliedPushForce = new Vector2(horizontalPushForce * basePushForce /(0.1f *  Mathf.Abs(player.transform.position.x-trans.position.x)), verticalPushForce * basePushForce / (0.1f * Mathf.Abs(player.transform.position.y - trans.position.y)));
+			player.GetComponent<Rigidbody2D>().AddForce(appliedPushForce);
+		}
+		Destroy(gameObject);
 	}
 
-	// Update is called once per frame
 	void Update () {
 	
 	}
